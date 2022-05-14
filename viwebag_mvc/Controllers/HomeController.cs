66ab4964a8dcf2fbcfca8446a822d4b29127bee2
User <User@DESKTOP-3571300>
2022-05-14 08:41:29 +0530

    Add project files.

---
+++ viwebag_mvc/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace viwebag_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.mssg1 = " That is viwebag masg";
+
+            ViewData["masg2"] = "that is viwedata";
+
+            ViewBag.curent = DateTime.Now.ToString();
+
+            ViewData["curent2date"] = DateTime.Now.ToString();
+
+            string[] cars = { "honda", "marutti", "bmw", "ford" };
+            ViewBag.cars = cars;
+            ViewData["car"] = cars;
+
+
+
+            return View();
+        }
+    }
+}
---
+++ viwebag_mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="viwebag_mvc.MvcApplication" Language="C#" %>
